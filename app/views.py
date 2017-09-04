9d7f97ce9ce1695db5aea12f4acd57d97548e227
Daniel Waterworth <da.waterworth@gmail.com>
2017-09-04 09:12:51 -0500

    Added more view things

--- app/views.py
+++ app/views.py
@@ -5,12 +5,26 @@ class Metric:
     def __init__(self):
         self.samples = {}
 
+    def get_data(self):
+        return self.samples
+
+    def __setitem__(self, key, value):
+        self.samples[key] = value
+
 class Run:
     def __init__(self):
         self.metrics = {}
 
     def __getitem__(self, metric_name):
         return self.metrics[metric_name]
+
+    def new_metric(self, metric_name):
+        metric = Metric()
+        self.metrics[metric_name] = metric
+        return metric
+
+    def metric_names(self):
+        return self.metrics.keys()
 
 class RunCollection:
     def __init__(self):
@@ -19,41 +33,50 @@ class RunCollection:
     def __getitem__(self, run_id):
         return self.runs[run_id]
 
+    def new_run(self):
+        self.runs.append(Run())
+        return len(self.runs) - 1
+
+    def run_ids(self):
+        return range(len(self.runs))
+
 runs = RunCollection()
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', runs=runs)
 
 @app.route('/run/<int:run_id>/', methods=['GET'])
 def run_index(run_id):
     run = runs[run_id]
-    raise NotImplementedError()
+    return render_template('run_index.html', runs=runs, run=run)
 
 @app.route('/run/<int:run_id>/metric/<string:metric_name>', methods=['GET'])
 def metrix_index(run_id, metric_name):
     run = runs[run_id]
     metric = run[metric_name]
-    raise NotImplementedError()
+    return render_template('metric_index.html', runs=runs, run=run, metric=metric)
 
 @app.route('/run/<int:run_id>/metric/<string:metric_name>/data.json', methods=['GET'])
 def metrix_data(run_id, metric_name):
     run = runs[run_id]
     metric = run[metric_name]
-    raise NotImplementedError()
+    return flask.json.jsonify({'samples': metric.get_data()})
 
 @app.route('/run/new', methods=['POST'])
-def index():
-    raise NotImplementedError()
+def new_run():
+    return flask.json.jsonify({'run_id': runs.new_run()})
 
 @app.route('/run/<int:run_id>/metric/<string:metric_name>/initialize', methods=['POST'])
-def index(run_id, metric_name):
+def initialize_metric(run_id, metric_name):
     run = runs[run_id]
-    metric = run[metric_name]
-    raise NotImplementedError()
+    run.new_metric(metric_name)
+    return ''
 
 @app.route('/run/<int:run_id>/metric/<string:metric_name>/data', methods=['POST'])
-def index(run_id, metric_name):
+def add_data(run_id, metric_name):
     run = runs[run_id]
     metric = run[metric_name]
-    raise NotImplementedError()
+    for key, value in request.json['samples'].items():
+        metric[key] = value
+    return ''
